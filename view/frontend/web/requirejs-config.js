e944d80256a76c390fc22aac4ac4871dcd3c3710
BeSthetic <33312714+BeSthetic@users.noreply.github.com>
2019-03-26 12:18:36 +0100

    Script link for "Read more" in search results

--- view/frontend/web/requirejs-config.js
+++ view/frontend/web/requirejs-config.js
@@ -1,15 +1,16 @@
 /*
 * @Author: Ngo Quang Cuong
 * @Date:   2016-12-21 03:15:18
-* @Last Modified by:   nquangcuong
-* @Last Modified time: 2016-12-22 04:50:29
+* @Last Modified by:   Abraham Osorio
+* @Last Modified time: 2019-03-26 10:52:18
 */
 
 var config = {
     map: {
         '*': {
             'js-question': 'PHPCuong_Faq/js/js-question',
-            'js-category': 'PHPCuong_Faq/js/js-category'
+            'js-category': 'PHPCuong_Faq/js/js-category',
+            'js-search': 'PHPCuong_Faq/js/js-search'
         }
     }
 };
